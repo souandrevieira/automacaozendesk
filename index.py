268b5b78732407698b837073dc32c1a293d0b801
André Leandro Vieira <134546903+souandrevieira@users.noreply.github.com>
2023-11-10 14:21:29 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.set_page_config(page_title="Meu site 1", layout="wide")
+
+st.title("Texto Maior para o titulo")
+st.subheader("segundo texto para um subtitulo")
+st.write("Testando um link [Link para o Google](http://www.google.com)")
+
+
+
+
